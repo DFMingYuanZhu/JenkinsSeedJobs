d76b1fdaabc1b7b5b3b9e022efb66c452bf044ca
zhumingyuan-2019 <zhumingyuan@buaa.edu.cn>
2023-06-08 21:26:38 -0400

    jenkins seed job

---
+++ src/jobs/seedjob_scan_repository.groovy
@@ -0,0 +1,22 @@
+multibranchPipline('fc-adm-ci') {
+    branchSources {
+        git {
+            id('fc-adm-ci')
+            remote('https://github.com/DFMingYuanZhu/SpringbootTest.git')
+            includes('main master develop')
+        }
+    }
+
+    orphaneItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+
+    configure {
+        it / 'trigger' << 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+            spec '* * * * *'
+            interval '60000'
+        }
+    }
+}
